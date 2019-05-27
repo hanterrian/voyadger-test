56e6f91d57e7ed4ca47c34e2fb2d32451c537d6c
hanterrian <hanterrian@ukr.net>
2019-05-28 01:11:57 +0300

    Init voyager

--- database/migrations/2016_01_01_000000_create_pages_table_2.php
+++ database/migrations/2016_01_01_000000_create_pages_table_2.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use TCG\Voyager\Models\Page;
 
-class CreatePagesTable extends Migration
+class CreatePagesTable2 extends Migration
 {
     /**
      * Run the migrations.
